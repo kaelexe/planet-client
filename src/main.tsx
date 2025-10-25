bc5747a510fd7bd71e719b2e7e1858b1a5ac2451
kaelexe <jamto@awesomecx.com>
2025-10-25 18:59:24 +0800

    w ant design

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App";
-import "./index.css";
+import "./assets/styles/index.css";
 import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
